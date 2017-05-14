dc33afacd7d70ea96371b3f0f5d62f331b805936
tdfirth <tom@stockopedia.com>
2017-05-14 11:18:21 +0100

    env rename

--- src/Lambda/Enironment.hs
+++
@@ -1,9 +0,0 @@
-module Lambda.Environment where
-
-import Lambda.Types
-
-import Data.HashTable.IO as H
-
-type HashTable k v = H.CuckooHashTable k v
-
-
---
+++ src/Lambda/Environment.hs
@@ -0,0 +1,7 @@
+module Lambda.Environment where
+
+import           Lambda.Types
+
+import qualified Data.Map.Strict as Map
+
+return
